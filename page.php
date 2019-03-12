8f9fbaa68341e622b05feaa2dd35634b771a8e7c
Griffin <pfenniger.griffin@gmail.com>
2019-03-11 17:45:38 -0700

    base changes

--- page.php
+++ page.php
@@ -1,15 +1,19 @@
 <?php require_once('header.php'); ?>
+<body>
+	<div class="wrapper">
+		<?php require_once('sidebar.php'); ?>
+		<div class="row main">
+			<div class="col-sm-12">
 
-	<div class="row">
-		<div class="col-sm-12">
+				<?php 
+					if ( have_posts() ) : while ( have_posts() ) : the_post();
+		
+						get_template_part( 'content', get_post_format() );
+	
+					endwhile; endif; 
+				?>
 
-			<?php 
-				if ( have_posts() ) : while ( have_posts() ) : the_post();
-  	
-					get_template_part( 'content', get_post_format() );
-  
-				endwhile; endif; 
-			?>
-
-		</div> <!-- /.col -->
-	</div> <!-- /.row -->
+			</div> <!-- /.col -->
+		</div> <!-- /.row -->
+	</div>
+</body>
